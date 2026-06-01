77feaf5b596b3f848562064d02ceedff93ca9bd7
chicote32 <elainecristina080775@gmail.com>
2026-06-01 18:27:07 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+import random 
+ale = int(random.random()*100)
+chances= 5
+limite_maior =100
+limite_menor = 0 
+def maior ():
+    global limite_menor
+    global limite_maior
+    global ale
+    global chances
+    if chances <= 0:
+        texto.config(text="Acabaram as chances!\nVoçe venceu")
+    else:
+        limite_menor = ale
+        ale = (limite_menor + limite_maior) // 2
+        texto.config(text=f"Seu número é {ale}")
+        chances = chances-1
+        texto2.config(text=f"vidas: {chances}")
+    
+def menor ():
+    global limite_menor
+    global limite_maior
+    global ale
+    global chances
+    if chances <= 0:
+        texto.config(text="Acabaram as chances!\nVoçe venceu")
+    else:
+        limite_maior = ale
+        ale = (limite_menor + limite_maior) // 2
+        texto.config(text=f"Seu número é {ale}")
+        chances = chances-1
+        texto2.config(text=f"vidas: {chances}")
+        
+def acertou ():
+    if chances < 0:
+        texto.config(text="Acabaram as chances!\nVoçe venceu")
+    else:
+        texto.config(text=f"acertamos seu numero é: {ale}")
+
+
+janela = tk.Tk()
+
+janela.title("vamos advinhar o seu numero em cinco chances")
+janela.geometry("400x300")
+texto = tk.Label(janela,text=f"seu numero é {ale}")
+texto.pack()
+texto2 = tk.Label(janela,text=f"vidas: {chances}")
+texto2.pack()
+botao = tk.Button(janela,text="maior",command=maior)
+botao.pack()
+botao2 = tk.Button(janela,text="menor",command=menor)
+botao2.pack()
+botao3 = tk.Button(janela,text="acertou",command=acertou)
+botao3.pack()
+texto.pack()
+janela.mainloop()
